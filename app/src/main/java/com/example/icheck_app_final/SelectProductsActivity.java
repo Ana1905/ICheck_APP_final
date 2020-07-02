b3d7684e7728a23b939607a7eb2b0f80c34775d0
Ana1905 <66711242+Ana1905@users.noreply.github.com>
2020-07-02 13:02:05 -0500

    final

--- app/src/main/java/com/example/icheck_app_final/SelectProductsActivity.java
+++ app/src/main/java/com/example/icheck_app_final/SelectProductsActivity.java
@@ -7,7 +7,6 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
 
-import fragments.HomeFragment;
 import fragments.SelectProductsFragment;
 
 public class SelectProductsActivity extends AppCompatActivity {
@@ -17,12 +16,12 @@ public class SelectProductsActivity extends AppCompatActivity {
         setContentView(R.layout.activity_select_products);
 
         FragmentManager fm = getSupportFragmentManager();
-        Fragment fragment = fm.findFragmentById(R.id.activity_select_products);
+        Fragment fragment = fm.findFragmentById(R.id.fragment_products_container);
 
         if (fragment == null){
             fragment = new SelectProductsFragment();
             fm.beginTransaction()
-                    .add(R.id.activity_select_products, fragment)
+                    .add(R.id.fragment_products_container, fragment)
                     .commit();
         }
     }
