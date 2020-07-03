75a8c2ac9559a1fab5b5bf3da6153d558d3ea456
David López <davidlopeztorres03@gmail.com>
2020-07-02 20:50:24 -0500

    Test V1.1

--- app/src/main/java/com/example/icheck_app_final/SelectProductsActivity.java
+++ app/src/main/java/com/example/icheck_app_final/SelectProductsActivity.java
@@ -1,13 +1,29 @@
 package com.example.icheck_app_final;
 
-import androidx.fragment.app.Fragment;
+import android.os.Bundle;
 
-import Abstract.SingleFragmentActivity;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+
 import fragments.SelectProductsFragment;
 
-public class SelectProductsActivity extends SingleFragmentActivity {
+public class SelectProductsActivity extends AppCompatActivity {
+
     @Override
-    protected Fragment getFragment() {
-        return new SelectProductsFragment();
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_select_products);
+
+        FragmentManager fm = getSupportFragmentManager();
+        Fragment fragment = fm.findFragmentById(R.id.fragment_products_container);
+
+        if (fragment == null) {
+            fragment = new SelectProductsFragment();
+            fm.beginTransaction()
+                    .add(R.id.fragment_products_container, fragment)
+                    .commit();
+        }
     }
 }
